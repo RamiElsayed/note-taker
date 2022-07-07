20de46c6896aa52a4c7bff04ea51a71ca5ac4053
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-07 18:24:57 +0100

    built helper functions

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const app = require('express');
-const path = require('path');
-const fs = require('fs'); 
+const app = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+const readFromFile = (filepath) => {
+  try {
+    const content = fs.readFileSync(filepath, "utf-8");
+    return JSON.parse(content);
+  } catch (error) {
+    throw new error.message();
+  }
+};
+
+const writeTofile = (filePath, data) => {
+  try {
+    fs.writeFileSync(filePath, data);
+  } catch (error) {
+    throw new error.message();
+  }
+};
+
+const readAndAppend = (data, filePath) => {
+  try {
+    const content = readFromFile(filePath);
+    content.psuh(data);
+    writeTofile(filePath, JSON.stringify(content));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
