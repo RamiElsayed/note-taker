1be769d71c82472ae2c0b011ae373e31b8b6f83b
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-12 17:30:51 +0100

    get notes api

--- server.js
+++ server.js
@@ -39,6 +39,20 @@ const createNewNote = (body, notes) => {
     }
 }
 
+app.post('api/notes', (req,res) => {
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
