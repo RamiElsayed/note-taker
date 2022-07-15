f0db8f0485d651370fb6fde160b0bf6a0ae8d5d0
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-15 12:33:56 +0100

    file name to public and used original code

--- server.js
+++ server.js
@@ -40,6 +40,15 @@ app.post('/api/notes', (req,res) => {
   res.json(note);
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 const deleteNote = (id, notes) => {
   for (let i = 0; i < notes.length; i++) {
